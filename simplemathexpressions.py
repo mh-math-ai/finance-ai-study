9bd9f9176e71d0ffa6289df0564f19ce61b88014
mh-math-ai <minhae.pak@gmail.com>
2026-02-13 20:46:46 +0900

    simple math expressions(arithmetic)

---
+++ simplemathexpressions.py
@@ -0,0 +1,18 @@
+# the four operations of arithmetic
+
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+number = 2 + 3 * 4
+print(number)
+number = number + 2 
+print (number)
+number += 2 # number = number + 2 와 동일
+print(number)
+number *= 2 # number = number * 2 와 동일
+print(number)
+number /= 2 # number = number / 2 와 동일
+print(number)
+number -= 2 # number = number - 2 와 동일
+print(number)
+number %= 5 # number = number % 5 와 동일
+print(number)
